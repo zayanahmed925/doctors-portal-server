eed1075c5b58107945171c26e1caa9aff3897c7c
zayanahmed925 <rjraju925@gmail.com>
2022-05-18 18:36:19 +0600

    get admin in all users

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ async function run() {
             }
 
         })
+        //Get admin
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
         //update 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
