fd58e4417ac6de28c6a34e9c918c6ff6fa5da311
zayanahmed925 <rjraju925@gmail.com>
2022-05-18 16:59:07 +0600

    make admin with verify jwt

--- index.js
+++ index.js
@@ -50,6 +50,16 @@ async function run() {
             const users = await userCollection.find().toArray();
             res.send(users)
         })
+        //make admin role
+        app.put('/user/admin/:email', verifyJwt, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
         //update 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
