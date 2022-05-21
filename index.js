971d05062e0335882fb8865a43d765daa9929597
zayanahmed925 <rjraju925@gmail.com>
2022-05-21 16:53:44 +0600

    create payment intent

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const app = express();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -50,6 +51,19 @@ async function run() {
                 res.status(403).send({ message: 'Forbidden Access' })
             }
         }
+
+        //For payment system
+        app.post('/create-payment-intent', verifyJwt, async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret });
+        })
 
 
         //Load all service
